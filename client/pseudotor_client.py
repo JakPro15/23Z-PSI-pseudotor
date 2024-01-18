333ca7b4213832098be9cad3488ff850f5476df8
kpalucki <91991025+Chr1sps@users.noreply.github.com>
2024-01-18 09:58:37 +0100

    Fixed the client continuing to search for nodes despite already being connected to one.

--- client/pseudotor_client.py
+++ client/pseudotor_client.py
@@ -1,19 +1,26 @@
-import ssl
 import socket
-from middle_node_chooser import choose_middle_node
+import ssl
 from contextlib import contextmanager
-from exceptions import MiddleNodeUnavailableError, ServerUnavailableError
 
+from exceptions import MiddleNodeUnavailableError, ServerUnavailableError
+from middle_node_chooser import choose_middle_node
 
 MIDDLE_NODE_PORT = 8000
 
 
 @contextmanager
-def pseudotor_wrap(connecting_socket: socket.socket, server_address: str, port: int, overseer_address: str):
+def pseudotor_wrap(
+    connecting_socket: socket.socket,
+    server_address: str,
+    port: int,
+    overseer_address: str,
+):
     connected = False
     unavailable_middle_nodes = set()
     while not connected:
-        middle_node = choose_middle_node(overseer_address, unavailable_middle_nodes)
+        middle_node = choose_middle_node(
+            overseer_address, unavailable_middle_nodes
+        )
         print(f"Selected middle node: {middle_node}")
         context = ssl.create_default_context()
         context.check_hostname = False
@@ -28,16 +35,17 @@ def pseudotor_wrap(connecting_socket: socket.socket, server_address: str, port: 
 
         try:
             data = bytearray(socket.inet_aton(server_address))
-            data.extend(port.to_bytes(2, 'big', signed=False))
+            data.extend(port.to_bytes(2, "big", signed=False))
             ssl_socket.sendall(data)
             print("Sent server address and port number")
             data = ssl_socket.recv(2)
         except Exception as e:
             unavailable_middle_nodes.add(middle_node)
             continue
-        if data == b'OK':
+        if data == b"OK":
             print("Connected to server.")
-        elif data == b'NO':
+            connected = True
+        elif data == b"NO":
             raise ServerUnavailableError("Failed to connect to the server.")
         else:
             unavailable_middle_nodes.add(middle_node)
